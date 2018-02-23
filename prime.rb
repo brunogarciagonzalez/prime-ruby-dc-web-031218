ebc48839dc621bbbe713923bf8a92314ebc6ec39
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-02-22 21:05:06 -0500

    lab completed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(n)
+  array = []
+
+  # must be 'a natural number greater than 1':
+  if n < 1 || n == 1
+    return false
+  end
+
+  for number in 1..1000 do
+    if n % number == 0
+      array.push(number)
+    end
+  end
+
+  array.each do |x|
+    if (x != 1) && (x != n)
+      return false
+    end
+  end
+
+  return true
+end
